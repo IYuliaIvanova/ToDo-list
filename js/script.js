5f67dee83e56109d96dbe7f07655b6325c3288f0
Yulia Ivanova <sheeeellby@gmail.com>
2021-12-09 00:20:23 +0300

    change: add block deleted; add id

--- js/script.js
+++ js/script.js
@@ -3,29 +3,39 @@ const drawList = (dataType, section) => {
 
   dataType.forEach((item) => {
     section.innerHTML += `    
-    <div class="card">
+    <div id="${item.id}" class="card">
       <span>Title:</span>
       <span class="title">${item.title}</span>
       <br/>
       <span>Description:</span>
       <span class="description">${item.description}</span>
       <br/>
-      <button class="editBtn">Edit</button>
-      <button class="deleteBtn">Delete</button>
-      <button class="nextBtn">Next</button>
-    </div>`;
+      <div>
+      ${
+        section.id === "todo" || section.id === "inProgress"
+          ? ` <button class="editBtn">Edit</button>
+            <button class="deleteBtn">Delete</button>
+            <button class="nextBtn">Next</button>
+          </div>`
+          : section.id === "done"
+          ? ` <button class="editBtn">Edit</button>
+            <button class="deleteBtn">Delete</button>
+          </div>`
+          : `</div>`
+      }</div>`;
   });
 };
 
-const deleteCard = (dataType, title, description) => {
+const deleteCard = (dataType, dataDelete, idCard, title, description) => {
   dataType.forEach((item, i) => {
-    if (item.title === title && item.description === description) {
+    if (Number(item.id) === Number(idCard)) {
+      dataDelete.push({ id: idCard, title: title, description: description });
       dataType.splice(i, 1);
     }
   });
 };
 
-const editCard = (dataType, title, description, section) => {
+const editCard = (dataType, idCard, title, description, section) => {
   const modalWrapper = document.querySelector(".wrapper");
   modalWrapper.style.display = "block";
 
@@ -43,8 +53,9 @@ const editCard = (dataType, title, description, section) => {
 
   submitBtn.addEventListener("click", () => {
     dataType.forEach((item, i) => {
-      if (item.title === title && item.description === description) {
+      if (Number(item.id) === Number(idCard)) {
         dataType.splice(i, 1, {
+          id: idCard,
           title: modalTitle.value,
           description: modalDescription.value,
         });
@@ -60,21 +71,23 @@ const nextCard = (
   dataTypeSplice,
   card,
   section,
+  idCard,
   title,
   description
 ) => {
-  dataTypePush.push({ title: title, description: description });
+  dataTypePush.push({ id: idCard, title: title, description: description });
   drawList(dataTypePush, section);
 
   card.remove();
   dataTypeSplice.forEach((item, i) => {
-    if (item.title === title && item.description === description) {
+    if (Number(item.id) === Number(idCard)) {
       dataTypeSplice.splice(i, 1);
     }
   });
 };
 
 const init = () => {
+  const wrapperData = document.querySelector(".wrapper-data");
   const todoList = document.querySelector(".todoList");
   const inputTitle = document.querySelector("#inputTitle");
   const inputDescription = document.querySelector("#inputDescription");
@@ -82,17 +95,20 @@ const init = () => {
   const form = document.querySelector("#form");
   const todoSection = document.querySelector("#todo");
   const inProgressSection = document.querySelector("#inProgress");
+  const deleteSection = document.querySelector("#deleted");
 
   const data = {
     todo: [],
     inProgress: [],
     done: [],
+    deleted: [],
   };
 
   addCardButton.addEventListener("click", (event) => {
     event.preventDefault(); //прерываение обновления формы по умолчанию !!! только с формой работает !!!
 
     data.todo.push({
+      id: Date.now(),
       title: inputTitle.value,
       description: inputDescription.value,
     });
@@ -101,27 +117,47 @@ const init = () => {
     drawList(data.todo, todoSection);
   });
 
-  todoList.addEventListener("click", (event) => {
+  wrapperData.addEventListener("click", (event) => {
     const card = event.target.closest(".card");
     const title = card.querySelector(".title").textContent;
     const description = card.querySelector(".description").textContent;
 
+    let cardSection = card.closest(".dataSection");
+    let idCard = card.id;
+    let idNext;
+
     switch (event.target.classList.value) {
       case "deleteBtn":
-        deleteCard(data.todo, title, description);
-        drawList(data.todo, todoSection);
+        deleteCard(
+          data[cardSection.id],
+          data.deleted,
+          idCard,
+          title,
+          description
+        );
+        drawList(data[cardSection.id], cardSection);
+        drawList(data.deleted, deleteSection);
         break;
 
       case "editBtn":
-        editCard(data.todo, title, description, todoSection);
+        editCard(data[cardSection.id], idCard, title, description, cardSection);
         break;
 
       case "nextBtn":
+        for (let i = 0; i < Object.keys(data).length; i++) {
+          if (Object.keys(data)[i] === cardSection.id) {
+            i += 1;
+            idNext = Object.keys(data)[i];
+          }
+        }
+        let nextSection = document.getElementById(idNext);
+
         nextCard(
-          data.inProgress,
-          data.todo,
+          data[idNext],
+          data[cardSection.id],
           card,
-          inProgressSection,
+          nextSection,
+          idCard,
           title,
           description
         );
